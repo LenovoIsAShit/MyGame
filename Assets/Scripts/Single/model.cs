a334311fab2d2622056ebedaad93cbc0fa7f0ec6
LenovoIsAShit <2352272505@qq.com>
2024-12-18 17:30:07 +0800

    666

---
+++ Assets/Scripts/Single/model.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 模型脚本
+///     保持模型方向和改变动作状态
+/// </summary>
+
+
+public class model : MonoBehaviour
+{
+    Transform mdl,came;
+    //模型
+    Animator ani;
+    
+    void Awake()
+    {
+        mdl = transform.GetChild(0);
+        came = GameObject.FindGameObjectWithTag("SingleCamera").transform;
+        ani = mdl.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        Local_camera cm = came.GetComponent<Local_camera>();
+        mdl.rotation = Quaternion.LookRotation(cm.Get_Forward());//初始朝向
+    }
+
+    private void Update()
+    {
+        Update_Model_State();
+    }
+
+    void LateUpdate()
+    {
+        Update_Dir();
+    }
+
+    ///////////////////    工具函数   ////////////////////////
+
+    void Update_Dir()
+    {
+        move mv=GetComponent<move>();
+        Vector3 md = mv.Get_movedir();
+        if (md.x != 0f || md.z != 0f) mdl.rotation = Quaternion.LookRotation(md);
+    }
+
+    void Update_Model_State()
+    {
+        bool running = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) ||
+                       Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
+
+        bool jumping = Input.GetKey(KeyCode.Space);
+
+        bool dance = Input.GetKey(KeyCode.R);
+
+        if (dance)
+        {
+            ani.SetBool("jump", false);
+            ani.SetBool("run", false);
+            ani.SetBool("dance", true);
+        }
+        else
+        {
+            ani.SetBool("dance", false);
+
+            if (running)
+            {
+                ani.SetBool("run", true);
+
+                if (jumping) ani.SetBool("rtj", true);
+                else ani.SetBool("rtj", false);
+            }
+            else
+            {
+                ani.SetBool("run", false);
+
+                if (jumping) ani.SetBool("jump", true);
+                else ani.SetBool("jump", false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Single/move.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 玩家移动脚本
+///     在摄像机执行awake，设置完位置后，根据摄像机与
+///     Player的相对位置得到前后左右方向，并移动
+/// </summary>
+
+public class move : MonoBehaviour
+{
+    Vector3 movedir, right, forward;
+    GameObject came;
+
+    public static float acceleration, speed;
+
+    void Awake()
+    {
+        acceleration = 3f;
+        speed = 0f;
+        movedir = new Vector3(0f, 0f, 0f);
+        forward = new Vector3(0f, 0f, 0f);
+        right = new Vector3(0f, 0f, 0f);
+        came = GameObject.FindGameObjectWithTag("SingleCamera");
+
+    }
+    void Start()
+    {
+        Set_Forward();
+        Set_Right();
+
+    }
+
+    void Update()
+    {
+        Speed_Judge();
+        //更新速度
+        Movedir_Judge();
+        //更新方向
+        Start_Move();
+        //移动
+    }
+
+    ///////////////////    工具函数   ////////////////////////
+
+    void Set_Forward()
+    {
+        forward = came.GetComponent<Local_camera>().Get_Forward();
+    }
+
+    void Set_Right()
+    {
+        right = came.GetComponent<Local_camera>().Get_Right();
+    }
+
+    void Speed_Judge()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            if (speed < acceleration)
+            {
+                speed += (Time.deltaTime * acceleration);
+            }
+        }
+        else
+        {
+            speed -= (Time.deltaTime * acceleration * 3f);
+            if (speed <= 0) speed = 0;
+        }
+    }
+    void Movedir_Judge()
+    {
+        if (Input.GetKey(KeyCode.W)) movedir += forward;
+        if (Input.GetKey(KeyCode.S)) movedir -= forward;
+        if (Input.GetKey(KeyCode.A)) movedir -= right;
+        if (Input.GetKey(KeyCode.D)) movedir += right;
+        movedir = movedir.normalized;
+    }
+    void Start_Move()
+    {
+        if (movedir.x != 0f || movedir.z != 0f) transform.rotation = Quaternion.LookRotation(movedir);
+        if(!Input.GetKey(KeyCode.R))transform.position += (speed * Time.deltaTime * movedir);
+    }
+
+    ///////////////////    外部接口   ////////////////////////
+
+    public Vector3 Get_movedir()
+    {
+        return movedir;
+    }
+}
